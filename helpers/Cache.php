46b8bf86296d681f754252d0837a49ab3b827e6a
Makis Tracend <makis.tracend@gmail.com>
2013-12-24 05:23:07 -0800

    initial commit

---
+++ helpers/Cache.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Cache
+ * Persistant cache for KISSCMS
+ * Created by Makis Tracend ( @tracend )
+ *
+ */
+
+if ( !class_exists("Cache") ){
+
+class Cache {
+
+	public function load(){
+
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+## KISSCMS: Cache
+
+Persistent cache plugin for KISSCMS
+
+
+##Credits
+
+Created by Makis Tracend (@tracend)
